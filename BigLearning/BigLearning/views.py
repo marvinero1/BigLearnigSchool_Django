8f406ae710dd1691b6ca42325b3639281bbd9565
marvinero <marvinero@gmail.com>
2019-10-07 17:52:52 -0400

    first commit

---
+++ BigLearning/BigLearning/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.template import Template, Context
+from django.template import loader
+from django.shortcuts import render
+
+def index(request):
+    
+    #doc_externo=loader.get_template('inicio.html')
+
+    #plantilla=Template(doc_externo.read())
+
+    #doc_externo.close()
+
+    #contexto=Context()
+
+    #documento=doc_externo.render()
+
+    return render(request, "index.html", None)
